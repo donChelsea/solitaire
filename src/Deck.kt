f2cbb6cd3cdc3f7a9ff377a5168ca344b54314fa
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-01-04 23:37:18 -0500

    created deck

---
+++ src/Deck.kt
@@ -0,0 +1,12 @@
+class Deck {
+
+    val cards: Array<Card> = Array(52, { Card(it % 13, getSuit(it))})
+
+    private fun getSuit(i: Int) = when (i / 13) {
+        0 -> "Clubs"
+        1 -> "Diamonds"
+        2 -> "Hearts"
+        else -> "Spades"
+    }
+
+}

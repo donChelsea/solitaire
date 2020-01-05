f16e7ce9390ad7c9d8063110c0dbfe122bc857ad
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-01-04 23:46:06 -0500

    added deck functions

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,6 +1,16 @@
+import java.util.*
+
 class Deck {
 
     val cards: Array<Card> = Array(52, { Card(it % 13, getSuit(it))})
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        cardsInDeck.shuffle()
+    }
 
     private fun getSuit(i: Int) = when (i / 13) {
         0 -> "Clubs"

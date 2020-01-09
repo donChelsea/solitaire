d44622b87677d446a48062b42c21e195fe54e967
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-01-09 16:24:51 -0500

    add game board

--- src/Card.kt
+++ src/Card.kt
@@ -6,9 +6,8 @@ val redSuits = arrayOf(diamonds, hearts)
 val blackSuits = arrayOf(clubs, spades)
 val cardMap = mapOf(0 to "A", 1 to "2", 2 to "3" , 3 to "4", 4 to "5", 5 to "6", 6 to "7", 7 to "8", 8 to "9", 9 to "10", 10 to "J", 11 to "Q", 12 to "K")
 
-
 data class Card(val value: Int, val suit: String, var faceUp: Boolean = false) {
-    override fun toString(): String = if (faceUp) "${cardMap[value]} ${getSuitChar(suit)}" else "xxx"
+    override fun toString(): String = if (faceUp) "${cardMap[value]}".padEnd(2) + "${getSuitChar(suit)}" else "xxx"
 
     private fun getSuitChar(suit: String): String = when (suit) {
         diamonds -> "\u2666"
